c53c82ce694d81c0d4110c66de0e98d017c93b3a
KaleTEvans <kaletevans@gmail.com>
2021-03-25 20:26:10 -0500

    added weather api call funciton to accept coordinate inputs

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+let getWeatherData = function(lat, lon) {
+    // format the api url
+    let weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=459a5e31598a1077257e521e66bb2960"
+
+    fetch(weatherApiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                // send to function to create the html
+                displayWeatherData(data);
+            });
+        }
+    });
+};
