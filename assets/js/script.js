d345ece40a609cacd7e9cf350146afd5b10db874
KaleTEvans <kaletevans@gmail.com>
2021-03-26 19:23:52 -0500

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,24 @@ let getWeatherData = function(lat, lon) {
         }
     });
 };
+
+// function to display all of the weather data on the page
+let displayWeatherData = function(data) {
+
+    // retrieve temperature
+    let currentTemp = data.current.temp;
+    // lt element = $('<element>').addClass('current-temp').text('The current temperature is: ' + currenTemp + ' F');
+
+    // retrieve humidity
+    let currentHumidity = data.current.humidity;
+    // let element = $('<element>').addClass('current-humudity').text('The current humidity is: ' + currentHumidity ' %');
+
+    // retrieve wind speed
+    let windSpeed = data.current.wind_speed;
+    // let element = $('<element>').addClass('current-wind').text('The current wind speed is: ' + windSpeed + 'MPH');
+
+    // retrieve uv index
+    let currentUv = data.current.uvi;
+    // let element = $('<element>').addClass('current-uv').text('The current UV index is: ' + currentUv);
+
+}
